60010fb14845272ed92c2fac67ef5cbe06727ae8
time682 <63047935+time682@users.noreply.github.com>
2025-11-25 15:07:46 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ API=29
 aarch64-linux-android${API}-clang++ \
     -Iinclude \
     main.cpp \
+    system_property_api.cpp \
     system_property_set.cpp \
     systemproperties/*.cpp \
     -std=c++17 \
@@ -12,7 +13,12 @@ aarch64-linux-android${API}-clang++ \
     -fvisibility=hidden -fno-rtti -fno-exceptions \
     -Wl,--gc-sections -s \
     -static-libgcc \
-    -o resetprop-arm64 && ls -al resetprop-arm64
+    -lc -lm -ldl -lz \
+    -stdlib=libc++ \
+    -static-libstdc++ \
+    -o resetprop-arm64
+
+ls -al resetprop-arm64
 
 # 修正动态库名称（保留）
 #sed -i -e 's@libc++_shared.so@libc++.so\x00\x00\x00\x00\x00\x00\x00@g' resetprop
